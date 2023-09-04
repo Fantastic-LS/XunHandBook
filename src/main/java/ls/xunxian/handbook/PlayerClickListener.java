78a2b4efbbb53dcc0fa11cf9b16ffef0719ddf54
皓廷 <1299857941@qq.com>
2023-09-04 14:51:29 +0800

    修复点击错误bug

--- src/main/java/ls/xunxian/handbook/PlayerClickListener.java
+++ src/main/java/ls/xunxian/handbook/PlayerClickListener.java
@@ -1,5 +1,6 @@
 package ls.xunxian.handbook;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -14,6 +15,9 @@ public class PlayerClickListener implements Listener {
     public void onPlayerClick(InventoryClickEvent event) {
         File[] files = XunConfigManager.getYamlFile();
         Inventory inventory = event.getClickedInventory();
+        Player player = (Player) event.getWhoClicked();
+        if (inventory == player.getInventory())
+            return;
         if (inventory.getTitle() != null) {
             String invName = inventory.getTitle();
             for (File file : files) {
